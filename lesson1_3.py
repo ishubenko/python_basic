922d1108e3dcf4badcb16046914b83dd99ff5f38
ishubenko <shubenkilja@gmail.com>
2020-04-09 16:06:50 +0300

    lesson1_3 done

--- lesson1_3.py
+++ lesson1_3.py
@@ -1,4 +1,15 @@
 n = input("Введите число n = ")
-n_x2 =
+n_string_x2 = n + n
+n_string_x3 = n + n + n
 
+num_1_and_2 = int(n_string_x2)
+num_1_and_2_and_3 = int(n_string_x3)
+
+sum = int(n) + num_1_and_2 + num_1_and_2_and_3
+
+print(sum)
+
+#print(n , "\n")
+#print(n_string_x2 , "\n")
+#print(n_string_x3 , "\n")
 #sum_n = n +
