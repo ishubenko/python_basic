1bdde420ba9a96a08663bf277ec5f1e199f9a5ce
ishubenko <shubenkilja@gmail.com>
2020-04-09 13:26:54 +0300

    Congratulations, finaly i did it

---
+++ lesson1_1.py
@@ -0,0 +1,11 @@
+#name = "Ilya"
+#surname = "Shubenko"
+#age = 34
+
+#print("Привет, ", name , surname , "\n")
+
+
+name = input("ВВедите Ваше имя\n")
+time_of_day = input("Сколько сейчас времени?\n")
+
+print("Привет, ", name)
