3f2fe78f16fe80409285c9270fc64cf3f3243413
ishubenko <shubenkilja@gmail.com>
2020-04-13 15:05:13 +0300

    less2_1 done

---
+++ lesson2_1.py
@@ -0,0 +1,15 @@
+test_list = [78,'simple string',48,'word',36.6,False,None]
+
+for itm in test_list:
+    print(itm,type(itm))
+
+#print(test_list[n])
+
+#print(test_list[1])
+#print(type(test_list[1]))
+
+#print(test_list[2])
+#print(type(test_list[2]))
+
+#print(test_list[3])
+#print(type(test_list[3]))
