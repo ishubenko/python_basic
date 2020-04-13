4464e1ffd59750e5f122e0ab7cff8a7a9a7d21fc
ishubenko <shubenkilja@gmail.com>
2020-04-13 12:13:40 +0300

    remove lesson1

--- lesson1_6.py
+++
@@ -1,9 +0,0 @@
-current = input('Первая дистанция, км : \n')
-target =  input('Цель пробежать, км : \n')
-i = 1
-
-while float(current) < int(target) and i < 10:
-    current = float(current) * 1.1
-    i += 1
-    print(current)
-print(i , 'дней Вам нужно, чтобы достигнуть цели')
