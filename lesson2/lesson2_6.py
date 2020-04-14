8c794730d8fa76c4e9c234c94a201a38bb4be604
ishubenko <shubenkilja@gmail.com>
2020-04-14 16:05:15 +0300

    repair les2_6

--- lesson2/lesson2_6.py
+++ lesson2/lesson2_6.py
@@ -1,29 +1,44 @@
 i=0
 goods_list =[]
-while i < 2:
+while i < 3:
     var_dict = {'название': input('Введите название товара:\n'),
                  'цена': int(input('Ввелите стоимость товара:\n')),
                  'колличество': int(input('Введите кол-во:\n')),
                  'ед.изм': input('Введите единицы измерения:\n')}
 #var_dict2 = {'название': 'принтер', 'цена': 6000, 'колличество': 2, 'ед.изм': 'шт'}
 #var_dict3 = {'название': 'сканер', 'цена': 2000, 'колличество': 7, 'ед.изм': 'шт'}
-    item_tuple = (i+1, var_dict)
+    item_tuple = (i, var_dict)
     goods_list.insert(i,item_tuple)
     i += 1
-print(goods_list)
-#item_tuple_2 = (2, var_dict2)
-#item_tuple_3 = (3, var_dict3)
-#goods_list = [item_tuple_1, item_tuple_2, item_tuple_3]
-#print(goods_list)
-#var_set1 = {var_dict1['название'],var_dict2['название'],var_dict3['название']}
-#var_set2 = {var_dict1['цена'],var_dict2['цена'],var_dict3['цена']}
-#var_set3 = {var_dict1['колличество'],var_dict2['колличество'],var_dict3['колличество']}
-#var_set4 = {var_dict1['ед.изм'],var_dict2['ед.изм'],var_dict3['ед.изм']}
+var_tuple1 = goods_list[0]
+var_tuple2 = goods_list[1]
+var_tuple3 = goods_list[2]
+#print(var_tuple1)
+#print(var_tuple2)
 
-#output_analize = {'название': var_set1,
+var_dict1 = (var_tuple1[1])
+var_dict2 = (var_tuple2[1])
+var_dict3 = (var_tuple3[1])
+#output_analize = {'название': goods_list[],
 #                  'цена': var_set2,
 #                  'количество': var_set3,
 #                  'ед.изм': var_set4
 #                  }
-#
 #print(output_analize)
+
+#item_tuple_2 = (2, var_dict2)
+#item_tuple_3 = (3, var_dict3)
+#goods_list = [item_tuple_1, item_tuple_2, item_tuple_3]
+#print(goods_list)
+var_set1 = {var_dict1['название'],var_dict2['название'],var_dict3['название']}
+var_set2 = {var_dict1['цена'],var_dict2['цена'],var_dict3['цена']}
+var_set3 = {var_dict1['колличество'],var_dict2['колличество'],var_dict3['колличество']}
+var_set4 = {var_dict1['ед.изм'],var_dict2['ед.изм'],var_dict3['ед.изм']}
+
+output_analize = {'название': var_set1,
+                  'цена': var_set2,
+                  'количество': var_set3,
+                  'ед.изм': var_set4
+                  }
+
+print(output_analize)
