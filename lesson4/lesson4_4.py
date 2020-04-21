98b0316222f1cf2d46e88657c8c44ed97efda506
ishubenko <shubenkilja@gmail.com>
2020-04-21 13:03:24 +0300

    remark lesson4_4

--- lesson4/lesson4_4.py
+++ lesson4/lesson4_4.py
@@ -1,16 +1,12 @@
+def my_func(number, itm):
+    out_1 = (i for i in number if i != itm)
+    return out_1
+
 var_list = [4, 2, 0, 4, 5]
 new_list = []
-FUNC
 
-for itm2 in var_list:
-    if itm2 != (itm for itm in var_list):
-        new_list.append(itm2)
+for itm in var_list:
+    my_func(new_list, itm)
+    new_list.append(itm)
 
 print(new_list)
-
- #   output_list = [itm for itm in var_list]
-
-
-#print(output_list)
-
-
