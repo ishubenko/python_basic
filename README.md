e89f9fa7e784ea1c4e89c1d3542868bef00cd5fb
ishubenko <shubenkilja@gmail.com>
2020-04-26 15:55:07 +0300

    del unused files

--- README.md
+++
@@ -1,3 +0,0 @@
-# Ветка для упражнений 4 урока
-
-Python lessons 07_04_2020 GeekBrains
--- my_module/script_1.py
+++
@@ -1,7 +0,0 @@
-def zarplata(hrs, rt, bns):
-    var_temp = (int(hrs) * int(rt)) + int(bns)
-    return var_temp
-
-
-#if __name__ == '__main__':
-#    print('привет')
